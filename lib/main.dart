034d8634a8d602d52ccee35852c314197ced3574
ShqipdonDrenica <63073692+ShqipdonDrenica@users.noreply.github.com>
2022-02-02 10:07:06 +0100

    finally

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //kthen nje widget qe eshte button qe pranon si parameter ngjyren dhe emrin e videos, te cilat i vendosim me vone kur ti therrasim
   Widget button({required Color color, required String soundNumber}) {
+    //krijimi i nje varible qe na mundeson qe te luhet voice(video)
     final player = new AudioCache();
+    //butoni i krijuar me madhesin e caktuar
     return FlatButton(
       height: 100,
       onPressed: () {
+        //kur behet click do te leshohet videoja me emrin note qe ka edhe numrin qe do te japim me vone kur e therrasim me endswith wav
         player.play("note$soundNumber.wav");
       },
       color: color,
@@ -38,6 +42,8 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          //krijimi i buttonave duke perdorur metoden me lart ku ja caktojm ngjyren per secilin button
+          //dhe bashkangjesim nr e emrit qe permban videoja
           button(color: Colors.red, soundNumber: "1"),
           button(color: Colors.blue, soundNumber: "2"),
           button(color: Colors.green, soundNumber: "3"),
